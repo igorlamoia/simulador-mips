76d5722840d4fff5ffd1cb3f02da625ce7cddc73
Igor Lamoia Queiroz <igorlamoia@hotmail.com>
2022-01-29 12:00:38 -0300

    ajustando helpers

--- Trabalho/src/helpers/Convercao.java
+++ Trabalho/src/helpers/Convercao.java
@@ -1,10 +1,24 @@
 package helpers;
+import java.math.BigInteger;
 
 public class Convercao {
-  public static String binParaHexa8(String binario) {
+  
+  public static String binToHex(String binario) {
     String hex = Integer.toString(Integer.parseInt(binario, 2), 16);
     for(int i = hex.length(); i < 8; i++)
             hex = "0"+hex;
-    return "0x"+hex;
-}
+    return hex;
+  }
+  
+  public static String hexToBin(String hex) {
+    return new BigInteger(hex.split("0x")[1], 16).toString(2);
+  }
+
+  public static String decimalToHex (int inteiro) {
+    return Integer.toString(inteiro, 16);
+  }
+
+  public static int hexToDec (String hex) {
+    return Integer.parseInt(hex,16);
+  }
 }
